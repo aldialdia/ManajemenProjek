161ec6aa2457209592c2a77f78c9d822b12cc628
RevinPahlevi <revinpahlevii@gmail.com>
2026-01-30 16:08:42 +0700

    superadmin

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Enums\UserRole;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -13,10 +14,21 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
+        // Create Super Admin account
+        User::create([
+            'name' => 'Super Admin',
+            'email' => 'superadmin@manajemenprojek.com',
+            'status' => 'active',
+            'role' => UserRole::SUPER_ADMIN,
+            'password' => bcrypt('admin123'),
+        ]);
+
+        // Create regular user account
         User::create([
             'name' => 'hakim',
             'email' => 'hakim@gmail.com',
             'status' => 'active',
+            'role' => UserRole::USER,
             'password' => bcrypt('revin123'),
         ]);
     }
